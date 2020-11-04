f2c064ecbb271dee5b45767427d47716f8e030fc
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-11-04 17:45:06 +0100

    Done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-
 using namespace std;
+
+string fizzBuzzParser(int);
+
 int main() {
-    printf("Hello Wolrd!");
+    for (int i = 1; i <= 100; ++i) {
+        cout << fizzBuzzParser(i) << endl;
+    }
     return 0;
+}
+
+string fizzBuzzParser(int i){
+    if (i%15 == 0){
+        return "FizzBuzz";
+    } else if (i%3 == 0){
+        return "Fizz";
+    } else if (i%5 == 0){
+        return "Buzz";
+    }
+    return to_string(i);
 }
